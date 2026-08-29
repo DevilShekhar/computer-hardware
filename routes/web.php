d66a6db008ea5f5888fe66692dbd9b94430aadc9
DevilShekhar <shekhar@eternalhightech.com>
2026-08-29 17:50:25 +0530

    web route updated

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\BuilderBrandController;
 use App\Http\Controllers\Admin\BuilderCategoryController;
-
-use App\Http\Controllers\BuilderBrandController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -19,16 +17,12 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('/users', UserController::class)->names('admin.users');
-    Route::resource('builder-brands', BuilderBrandController::class);
-    Route::resource('builder-categories', BuilderCategoryController::class);
-});
-});
-
-Route::middleware('auth')->group(function () {
     Route::resource('roles', RoleController::class);
     Route::get('roles/{role}/permissions-data', [RoleController::class, 'getPermissionsData'])->name('roles.permissions.data');
     Route::get('roles/{role}/permissions', [RoleController::class, 'managePermissions'])->name('roles.permissions');
     Route::put('roles/{role}/permissions', [RoleController::class, 'updatePermissions'])->name('roles.permissions.update');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/users', UserController::class)->names('admin.users');
+    Route::resource('builder-brands', BuilderBrandController::class);
+    Route::resource('builder-categories', BuilderCategoryController::class);
 });
