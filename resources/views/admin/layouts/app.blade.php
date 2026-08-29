719a09de558b08e4a7269aa37fe2d6aa5c68abe3
DevilShekhar <shekhar@eternalhightech.com>
2026-08-29 16:26:12 +0530

    route added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -184,17 +184,31 @@
           </div>
           <ul class="sidebar-menu">
             <li class="menu-header">Main</li>
-            <li class="dropdown active">
-              <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            <li class="{{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                <a href="{{ route('dashboard') }}" class="nav-link">
+                    <i data-feather="monitor"></i>
+                    <span>Dashboard</span>
+                </a>
             </li>
-            <li class="dropdown">
-              <a href="#" class="menu-toggle nav-link has-dropdown"><i
-                  data-feather="briefcase"></i><span>Widgets</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
-                <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
-              </ul>
+            <li class="dropdown {{ request()->routeIs('admin.users.*') ? 'active' : '' }}">
+                <a href="#" class="menu-toggle nav-link has-dropdown">
+                    <i data-feather="users"></i>
+                    <span>Users</span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="{{ request()->routeIs('admin.users.index') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.users.index') }}">
+                            User Listing
+                        </a>
+                    </li>
+                    <li class="{{ request()->routeIs('admin.users.create') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.users.create') }}">
+                            Add User
+                        </a>
+                    </li>
+                </ul>
             </li>
+
             <li class="dropdown">
               <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="command"></i><span>Apps</span></a>
               <ul class="dropdown-menu">
