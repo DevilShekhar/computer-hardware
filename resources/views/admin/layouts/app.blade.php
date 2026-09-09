b8696316c040d12f16b21ec5a13f31da94020da4
DevilShekhar <shekhar@eternalhightech.com>
2026-09-09 14:35:00 +0530

    dashboard count added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Otika - Admin Dashboard Template</title>
+    <title>Metaverse Info - Dashboard </title>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/bundles/summernote/summernote-bs4.css') }}">
@@ -21,7 +21,7 @@
         href="{{ asset('assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/select-2.css') }}">
 
-    <link rel='shortcut icon' type='image/x-icon' href="{{ asset('assets/img/favicon.ico') }}" />
+    <link rel='shortcut icon' type='image/x-icon' href="{{ asset('assets/img/favicon.png') }}" />
     <style>
         .select2-container {
             width: 100% !important
@@ -69,6 +69,9 @@
 
         .select2-container--default .select2-results__option--highlighted[aria-selected] {
             background: #6777ef
+        }
+        .sidebar-brand a img {
+            height: 90px;
         }
     </style>
 </head>
@@ -248,8 +251,7 @@
             <div class="main-sidebar sidebar-style-2">
                 <aside id="sidebar-wrapper">
                     <div class="sidebar-brand">
-                        <a href="index.html"> <img alt="image" src="assets/img/logo.png" class="header-logo" /> <span
-                                class="logo-name">Otika</span>
+                        <a href="{{ route('dashboard') }}"> <img alt="image" src="{{ asset('assets/img/logo.png') }}" />
                         </a>
                     </div>
                     <ul class="sidebar-menu">
