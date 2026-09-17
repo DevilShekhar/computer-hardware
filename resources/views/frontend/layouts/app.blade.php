727db92fea2a8fba97def4993704937474c07800
Abhishek-10hue <ehtabhishek@eternalhightech.com>
2026-09-17 12:49:01 +0530

    renmed the named

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -39,7 +39,7 @@
     <!-- Helper CSS -->
     <link rel="stylesheet" href="{{ asset('assets/frontend/assets/css/helper.css') }}">
     <!-- Main Style CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/frontend/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/frontend/assets/css/style-new.css') }}">
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="{{ asset('assets/frontend/assets/css/responsive.css') }}">
     <!-- Modernizr JS -->
